563b99f947627c6f366a53da34d05589a2d41ad6
Aaron Rasing <mrasing@ewise.com>
2018-03-08 00:07:31 +0800

    Chapter 4 Done : Circuit Breakers

--- hystrix-demo-dashboard/src/main/java/org/pipecrafts/practice/pluralsight/HystrixDemoDashboardApplication.java
+++ hystrix-demo-dashboard/src/main/java/org/pipecrafts/practice/pluralsight/HystrixDemoDashboardApplication.java
@@ -3,12 +3,13 @@ package org.pipecrafts.practice.pluralsight;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableHystrixDashboard
-public class HystrixDashboardApplication {
+public class HystrixDemoDashboardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HystrixDashboardApplication.class, args);
+		SpringApplication.run(HystrixDemoDashboardApplication.class, args);
 	}
 }
--- hystrix-demo-dashboard/src/test/java/org/pipecrafts/practice/pluralsight/HystrixDemoDashboardApplicationTests.java
+++ hystrix-demo-dashboard/src/test/java/org/pipecrafts/practice/pluralsight/HystrixDemoDashboardApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class HystrixDashboardApplicationTests {
+public class HystrixDemoDashboardApplicationTests {
 
 	@Test
 	public void contextLoads() {
